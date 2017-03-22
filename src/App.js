9b3c39eeadbf73295f6a87c8b9b3a38dbaa527cc
Peter Munro <peter@petermunro.org>
2017-03-22 15:55:34 +0000

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -3,44 +3,12 @@ import ContactListContainer from './ContactListContainer';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.tellMeWhenExpandedIsHit = this.tellMeWhenExpandedIsHit.bind(this);
-  }
-
   render() {
-    const mockContacts = [
-      {
-        name: "Peter Munro",
-        address: {
-          line1: '16 The Harbor',
-          town: 'Newport',
-          county: 'Gwent',
-          country: 'Wales'
-        },
-        email: "peter@petermunro.org",
-      },
-      {
-        name: "Dave Mallon",
-        address: {
-          line1: '5 The Villas',
-          town: 'Stourbridge',
-          county: 'Devon',
-          country: 'England'
-        },
-        email: "dave@example.com",
-      }
-    ];
-
     return (
       <div className="App">
-        <ContactListContainer contacts={mockContacts} />
+        <ContactListContainer />
       </div>
     );
-  }
-
-  tellMeWhenExpandedIsHit(component, isExpanded) {
-    console.log(`Component ${component} ${isExpanded ? "is" : "is not"} expanded`);
   }
 
 }
